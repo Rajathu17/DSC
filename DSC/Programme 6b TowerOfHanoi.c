ae7323447da72d581a2e045d6f6f035c4955a343
Rajathu17 <130749215+Rajathu17@users.noreply.github.com>
2024-03-31 17:49:14 +0530

    Update Programme 6b TowerOfHanoi.c

--- DSC/Programme 6b TowerOfHanoi.c
+++ DSC/Programme 6b TowerOfHanoi.c
@@ -6,16 +6,16 @@ int main() {
     int n;
     printf("Enter the number of rings: ");
     scanf("%d", &n);
-    ToH(n, 'A', 'B', 'C');
+    ToH(n, 'A', 'C', 'B');
     return 0;
 }
 
-void ToH(int n, char A, char B, char C) {
+void ToH(int n, char A, char C, char B) {
     if (n == 1) {
         printf("Move ring 1 from %c to %c\n", A, C);
         return;
     }
-    ToH(n - 1, A, C, B);
+    ToH(n - 1, A, B, C);
     printf("Move ring %d from %c to %c\n", n, A, C);
-    ToH(n - 1, B, A, C);
+    ToH(n - 1, B, C, A);
 }
